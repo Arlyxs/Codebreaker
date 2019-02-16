c16605f3c11b88a2f07de854eff2f6e978b9e573
Aleix Culture <you@example.com>
2019-02-15 22:52:56 -0400

    solved assignment

--- codebreaker.py
+++ codebreaker.py
@@ -1,27 +1,39 @@
 import random
 digits = list(range(10))
 random.shuffle(digits)
-digits_str = str(digits[:3])
-print(digits_str)
 print(digits[:3])
 
 guess = []
 tries = 0
-while str(guess) != digits_str:
+while guess != digits[:3]:
     number = input('What is your 3 digit guess?')
-    
+
     tries +=1
     for elem in number:
         guess.append(int(elem))
-        guess_str=str(guess)
-        print(guess_str)
+        print(guess)
 
-    if guess_str not in digits_str:
-        print("wrong guess")
+    if guess == digits[:3]:
+        print('you win')
+    elif guess[0] == digits[:3][0]:
+        print('you guessed a correct number in the right position')
         guess.clear()
-    #elif guess_str in digits_str:
-      #  print('you win')
+    elif guess[1] == digits[:3][1]:
+        print('you guessed a correct number in the right position')
+        guess.clear()
+    elif guess[2] == digits[:3][2]:
+        print('you guessed a correct number in the right position')
+        guess.clear()
+    elif guess[0] in digits[:3] and guess[0] != digits[:3][0]:
+        print('you guessed a correct number in the wrong position')
+        guess.clear()
+    elif guess[1] in digits[:3] and guess[1] != digits[:3][1]:
+        print('you guessed a correct number in the wrong position')
+        guess.clear()
+    elif guess[2] in digits[:3] and guess[2] != digits[:3][2]:
+        print('you guessed a correct number in the wrong position')
+        guess.clear()   
     else:
-        print('you got it')
-    
+        print("you haven't guessed any of the numbers correctly")
+        guess.clear()
     print ('number of tries {val} '.format(val=tries))
--- test.py
+++ test.py
@@ -1,25 +1,39 @@
 import random
 digits = list(range(10))
 random.shuffle(digits)
-digits_str = str(digits[:3])
-print(digits_str)
 print(digits[:3])
 
 guess = []
 tries = 0
-number = input('What is your 3 digit guess? ')
-247
-tries = tries + 1
-for elem in number:
-    guess.append(int(elem))
-print(guess)
+while guess != digits[:3]:
+    number = input('What is your 3 digit guess?')
 
-if guess > digits[:3]:
-        print("too high")
-elif guess < digits[:3]:
-        print('too low')
-else:
-        print('you got it')
-guess.clear()
-print ('number of tries ' + str(tries))
+    tries +=1
+    for elem in number:
+        guess.append(int(elem))
+        print(guess)
 
+    if guess == digits[:3]:
+        print('you win')
+    elif guess[0] == digits[:3][0]:
+        print('you guessed a correct number in the right position')
+        guess.clear()
+    elif guess[1] == digits[:3][1]:
+        print('you guessed a correct number in the right position')
+        guess.clear()
+    elif guess[2] == digits[:3][2]:
+        print('you guessed a correct number in the right position')
+        guess.clear()
+    elif guess[0] in digits[:3] and guess[0] != digits[:3][0]:
+        print('you guessed a correct number in the wrong position')
+        guess.clear()
+    elif guess[1] in digits[:3] and guess[1] != digits[:3][1]:
+        print('you guessed a correct number in the wrong position')
+        guess.clear()
+    elif guess[2] in digits[:3] and guess[2] != digits[:3][2]:
+        print('you guessed a correct number in the wrong position')
+        guess.clear()   
+    else:
+        print("you haven't guessed any of the numbers correctly")
+        guess.clear()
+    print ('number of tries {val} '.format(val=tries))
